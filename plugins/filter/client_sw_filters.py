b3e22482d2b43cdfe399d3029e8c7e6b76ea14cf
Mark Stillings <mark.stillings@quest.com>
2020-07-01 20:12:36 +0000

    Have basic handling of both INI and simple config file types working.

--- plugins/filter/client_sw_filters.py
+++ plugins/filter/client_sw_filters.py
@@ -21,6 +21,7 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 from ansible.module_utils.common._collections_compat import Mapping
+from ansible.errors import AnsibleFilterError
 
 
 # ------------------------------------------------------------------------------
